2ebc32f98ee2a45e7de57ff045173ff0b0f65f25
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-03-17 16:34:52 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Header from "./Layout/Header/Header";
 import Hero from "./Pages/Hero/Hero";
 import About from "./Pages/About/About";
+import Roadmap from "./Pages/Roadmap/Roadmap";
 function App() {
   return (
     <div className="App">
       <Header />
       <Hero />
       <About />
+      <Roadmap />
     </div>
   );
 }
